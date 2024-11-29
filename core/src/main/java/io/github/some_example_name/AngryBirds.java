e5503deec93f98a33550625cde7e906ceb631923
Aditya Nuli <aditya15nuli@gmail.com>
2024-11-29 17:09:59 +0530

    Soundtrack added

--- core/src/main/java/io/github/some_example_name/AngryBirds.java
+++ core/src/main/java/io/github/some_example_name/AngryBirds.java
@@ -1,20 +1,23 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import screens.MainGameScreen;
 import screens.MainMenuScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class AngryBirds extends Game {
     public SpriteBatch batch;
-
+    private Music soundtrack;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        soundtrack = Gdx.audio.newMusic(Gdx.files.internal("soundtrack.mp3"));
+        soundtrack.setLooping(true);
+        soundtrack.play();
         this.setScreen(new MainMenuScreen(this));
-
     }
 
     @Override
@@ -24,5 +27,7 @@ public class AngryBirds extends Game {
 
     @Override
     public void dispose() {
+        batch.dispose();
+        soundtrack.dispose();
     }
 }
